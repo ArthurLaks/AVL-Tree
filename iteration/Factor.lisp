76bc3a5b5050eee413cd546e9e397684058b8303
ArthurLaks <aryeh@laksfamily.com>
2015-09-09 09:19:21 -0400

    Deleted files not relevant to the AVL tree

--- iteration/Factor.lisp
+++
@@ -1,2 +0,0 @@
-(defun factor (number) (let ((factors (quote ()))) (dotimes (counter number factors) (if (= (mod number (+ counter 1)) 0) (setf factors (cons (+ counter 1)factors))))))
-(print (factor 210))
