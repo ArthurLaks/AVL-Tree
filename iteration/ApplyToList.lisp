c2511fc5ee948a55e6416230cf20c4b345a35639
ArthurLaks <aryeh@laksfamily.com>
2015-09-09 09:19:50 -0400

    Deleted files not relevant to the AVL tree

--- iteration/ApplyToList.lisp
+++
@@ -1,5 +0,0 @@
-(defun apply-to-list (list function)
-"As far as I can tell, this function is identical to mapcar"
-  (let (retval) 
-    (dolist (citem list retval)
-	    (push (funcall function citem) retval))))
